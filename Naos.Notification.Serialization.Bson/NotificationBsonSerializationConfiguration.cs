c4701f040baed1dbe0e86a38f46316aa1aa296d0
Suraj Gupta <SurajGupta@users.noreply.github.com>
2021-02-01 11:48:45 -0500

    seed Domain and associated projects

---
+++ Naos.Notification.Serialization.Bson/NotificationBsonSerializationConfiguration.cs
@@ -0,0 +1,41 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="NotificationBsonSerializationConfiguration.cs" company="Naos Project">
+//    Copyright (c) Naos Project 2019. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.Notification.Serialization.Bson
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Naos.Protocol.Serialization.Bson;
+    using OBeautifulCode.Serialization.Bson;
+    using OBeautifulCode.Type;
+    using OBeautifulCode.Type.Recipes;
+
+    /// <inheritdoc />
+    public class NotificationBsonSerializationConfiguration : BsonSerializationConfigurationBase
+    {
+        /// <inheritdoc />
+        protected override IReadOnlyCollection<string> TypeToRegisterNamespacePrefixFilters =>
+            new[]
+            {
+                Naos.Notification.Domain.ProjectInfo.Namespace,
+            };
+
+        /// <inheritdoc />
+        protected override IReadOnlyCollection<BsonSerializationConfigurationType> DependentBsonSerializationConfigurationTypes =>
+            new[]
+            {
+                typeof(ProtocolBsonSerializationConfiguration).ToBsonSerializationConfigurationType(),
+            };
+
+        /// <inheritdoc />
+        protected override IReadOnlyCollection<TypeToRegisterForBson> TypesToRegisterForBson => new Type[0]
+            .Concat(new[] { typeof(IModel) })
+            .Concat(Naos.Notification.Domain.ProjectInfo.Assembly.GetPublicEnumTypes())
+            .Select(_ => _.ToTypeToRegisterForBson())
+            .ToList();
+    }
+}
---
+++ Naos.Notification.Serialization.Bson/Properties/AssemblyInfo.cs
@@ -0,0 +1,43 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Naos Project">
+//    Copyright (c) Naos Project 2019. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Naos.Notification.Serialization.Bson")]
+[assembly: AssemblyDescription("Naos.Notification.Serialization.Bson")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Naos Project")]
+[assembly: AssemblyProduct("Naos.Notification.Serialization.Bson")]
+[assembly: AssemblyCopyright("Copyright © Naos Project 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9c3cc59a-7933-431d-b433-936393cd6ff5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: CLSCompliant(true)]
