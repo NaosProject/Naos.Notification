fa5f6c179abbe22df7cdd53029f13f5dbca14e72
Suraj Gupta <SurajGupta@users.noreply.github.com>
2021-02-04 14:13:55 -0500

    move Audience from NotificationBase to AudienceSpecifiedNotificationBase

---
+++ Naos.Notification.Domain/Models/Classes/Notification/AudienceSpecifiedNotificationBase.cs
@@ -0,0 +1,35 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AudienceSpecifiedNotificationBase.cs" company="Naos Project">
+//    Copyright (c) Naos Project 2019. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.Notification.Domain
+{
+    using OBeautifulCode.Assertion.Recipes;
+    using OBeautifulCode.Type;
+
+    /// <summary>
+    /// Base class for notification to use when the system cannot determine the audience from the notification itself.
+    /// </summary>
+    // ReSharper disable once RedundantExtendsListEntry
+    public abstract partial class AudienceSpecifiedNotificationBase : NotificationBase, IModelViaCodeGen
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AudienceSpecifiedNotificationBase"/> class.
+        /// </summary>
+        /// <param name="audience">Audience to send the notification to.</param>
+        protected AudienceSpecifiedNotificationBase(
+            IAudience audience)
+        {
+            new { audience }.AsArg().Must().NotBeNull();
+
+            this.Audience = audience;
+        }
+
+        /// <summary>
+        /// Gets the audience to send the notification to.
+        /// </summary>
+        public IAudience Audience { get; private set; }
+    }
+}
--- Naos.Notification.Domain/Models/Classes/Notification/NotificationBase.cs
+++ Naos.Notification.Domain/Models/Classes/Notification/NotificationBase.cs
@@ -13,19 +13,5 @@ namespace Naos.Notification.Domain
     /// </summary>
     public abstract partial class NotificationBase : INotification, IModelViaCodeGen
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="NotificationBase"/> class.
-        /// </summary>
-        /// <param name="audience">OPTIONAL audience to send the notification to.  DEFAULT is null.  Should be set if the system cannot determine the audience from the notification itself.</param>
-        protected NotificationBase(
-            IAudience audience = null)
-        {
-            this.Audience = audience;
-        }
-
-        /// <summary>
-        /// Gets the audience to send the notification to.
-        /// </summary>
-        public IAudience Audience { get; private set; }
     }
 }
